847fda8f3631001b48a8d7e85d0a9363899374fc
RiccardoBernardi1 <rikyb3004@gmail.com>
2023-01-18 14:18:46 +0100

    Mostra solo i treni in partenza da oggi in poi

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
 use App\Models\Train;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon as SupportCarbon;
 
 class PageController extends Controller
 {
     public function index() {
-        $trains=Train::all();
+        $trains=Train::where('departure_time','>=',now())->get();
         return view('homepage',compact('trains'));
     }
 }
